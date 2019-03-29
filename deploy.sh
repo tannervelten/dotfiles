ba114821c206b3f936f77f3a0289a8fb597edd90
Tanner Velten <tannervelten@gmail.com>
2019-03-29 10:02:47 -0700

    tmux, updates deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
 # TODO: use source instead of symlink
 
+# dependencies
+brew install zsh
+brew install tmux
+
+# vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# symlinks
 ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
